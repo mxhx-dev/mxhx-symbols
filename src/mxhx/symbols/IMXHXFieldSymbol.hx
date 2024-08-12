2da516ebd60f25526d27c807571ceaa35026760f
Josh Tynjala <joshtynjala@bowlerhat.dev>
2024-08-12 14:55:24 -0700

    IMXHXFieldSymbol: parent property to get the IMXHXTypeSymbol
    Similar to parent on IMXHXEnumFieldSymbol

--- src/mxhx/symbols/IMXHXFieldSymbol.hx
+++ src/mxhx/symbols/IMXHXFieldSymbol.hx
@@ -19,6 +19,11 @@ package mxhx.symbols;
 **/
 interface IMXHXFieldSymbol extends IMXHXSymbol {
 	/**
+		The field's owning type.
+	**/
+	public var parent:IMXHXTypeSymbol;
+
+	/**
 		The field's type.
 	**/
 	public var type:IMXHXTypeSymbol;
--- src/mxhx/symbols/internal/MXHXFieldSymbol.hx
+++ src/mxhx/symbols/internal/MXHXFieldSymbol.hx
@@ -4,6 +4,7 @@ import mxhx.symbols.IMXHXTypeSymbol;
 import mxhx.symbols.IMXHXFieldSymbol;
 
 class MXHXFieldSymbol implements IMXHXFieldSymbol {
+	public var parent:IMXHXTypeSymbol;
 	public var name:String;
 	public var doc:Null<String>;
 	public var file:String;
@@ -16,8 +17,9 @@ class MXHXFieldSymbol implements IMXHXFieldSymbol {
 	public var isWritable:Bool;
 	public var meta:Array<{name:String, ?params:Array<String>}>;
 
-	public function new(name:String, ?type:IMXHXTypeSymbol, isMethod:Bool = false, isPublic:Bool = true, isStatic:Bool = false) {
+	public function new(name:String, ?parent:IMXHXTypeSymbol, ?type:IMXHXTypeSymbol, isMethod:Bool = false, isPublic:Bool = true, isStatic:Bool = false) {
 		this.name = name;
+		this.parent = parent;
 		this.type = type;
 		this.isMethod = isMethod;
 		this.isPublic = isPublic;
